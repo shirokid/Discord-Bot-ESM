598c66a7d948846f9b66991a303a2c261c38d352
shirokid <raghavthedev@gmail.com>
2022-10-16 17:16:50 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -3,16 +3,14 @@ console.clear();
 
 import Bot from '../classes/Bot';
 
-let intents = ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS', 'GUILD_PRESENCES', 'GUILD_EMOJIS_AND_STICKERS', 'GUILD_VOICE_STATES'];
-let partials = ['USER', 'GUILD_MEMBER', 'MESSAGE', 'REACTION'];
 const client = new Bot({
-  intents,
-  partials,
+  intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS', 'GUILD_PRESENCES', 'GUILD_EMOJIS_AND_STICKERS', 'GUILD_VOICE_STATES'],
+  partials: ['USER', 'GUILD_MEMBER', 'MESSAGE', 'REACTION'],
   token: process.env.TOKEN,
   prefix: process.env.PREFIX,
   developerId: process.env.DEV,
   handlers: ['event', 'command', 'button'],
-  storage: './database.json'
+  storage: './database.json',
 });
 
 client.run();
